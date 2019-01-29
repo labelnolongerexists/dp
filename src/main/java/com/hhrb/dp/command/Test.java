923a544e24437f7bf90bf8b9ae73795a4c0eee02
labelnolongerexists <labelnolongerexists@gmail.com>
2019-01-30 00:13:04 +0800

    Init commit

---
+++ src/main/java/com/hhrb/dp/command/Test.java
@@ -0,0 +1,10 @@
+package com.hhrb.dp.command;
+
+public interface Test {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
